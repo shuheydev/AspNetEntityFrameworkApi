38a94bf379d9697fb634cd9fd12609c4d31872c8
Shuhey <shuhey.dev@gmail.com>
2020-04-05 16:33:34 +0900

    コミット

--- AspNetEntityFrameworkApi/Controllers/WeatherForecastController.cs
+++ AspNetEntityFrameworkApi/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace AspNetEntityFrameworkApi.Controllers
         {
             _logger = logger;
         }
-
+        
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
